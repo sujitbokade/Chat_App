441cb3741bdbdb4f467912f11458396e1e0a3fc3
sujit <sujitbokade2014@gmail.com>
2023-04-28 13:02:10 +0530

    [Sujit]Chat App

--- server/index.js
+++ server/index.js
@@ -24,38 +24,38 @@ socketIO.on('connection', socket => {
 
   socket.on('createRoom', roomName => {
     socket.join(roomName);
-    //👇🏻 Adds the new group name to the chat rooms array
+    // Adds the new group name to the chat rooms array
     chatRooms.unshift({id: generateID(), roomName, messages: []});
-    //👇🏻 Returns the updated chat rooms via another event
+    // Returns the updated chat rooms via another event
     socket.emit('roomsList', chatRooms);
   });
 
   socket.on('findRoom', id => {
-    //👇🏻 Filters the array by the ID
+    // Filters the array by the ID
     let result = chatRooms.filter(room => room.id === id);
-    //👇🏻 Sends the messages to the app
+    //Sends the messages to the app
     socket.emit('foundRoom', result[0].messages);
   });
 
   socket.on('newMessage', data => {
-    //👇🏻 Destructures the property from the object
+    // Destructures the property from the object
     const {room_id, message, user, timestamp} = data;
 
-    //👇🏻 Finds the room where the message was sent
+    // Finds the room where the message was sent
     let result = chatRooms.filter(room => room.id === room_id);
 
-    //👇🏻 Create the data structure for the message
+    // Create the data structure for the message
     const newMessage = {
       id: generateID(),
       text: message,
       user,
       time: `${timestamp.hour}:${timestamp.mins}`,
     };
-    //👇🏻 Updates the chatroom messages
+    // Updates the chatroom messages
     socket.to(result[0].name).emit('roomMessage', newMessage);
     result[0].messages.push(newMessage);
 
-    //👇🏻 Trigger the events to reflect the new changes
+    // Trigger the events to reflect the new changes
     socket.emit('roomsList', chatRooms);
     socket.emit('foundRoom', result[0].messages);
   });
